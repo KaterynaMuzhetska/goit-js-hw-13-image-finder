3b62f9a620588beb775cb9f92fdf707b53c52faf
KaterynaMuzhetska <74104436+KaterynaMuzhetska@users.noreply.github.com>
2021-05-31 22:54:24 +0300

    HW-13

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
+import './index.css';
+
 import CardApiService from './apiService';
+import imgTmpl from './imageCard.hbs';
 
 
 const cardApiService = new CardApiService();
+const galleryEl = document.querySelector('.gallery');
+const btnEl = document.querySelector('[data-action="load-more"]');
 
+btnEl.classList.add('is-hidden');
 
 function searchImages(event) {
     event.preventDefault();
-
-    //clearHitsContainer()
+    clearAImgContainer();
     cardApiService.searchQuery = event.currentTarget.elements.query.value;
     cardApiService.page=1;
-    cardApiService.getImages().then(response => {
-        console.log(response);
+    cardApiService.getImages().then(imgMarkup);
+    btnEl.addEventListener('click', onLoadMore);
+    btnEl.classList.remove('is-hidden');
+}
+
+function imgMarkup(hits) {
+    galleryEl.insertAdjacentHTML('beforeend', imgTmpl(hits));
+    galleryEl.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
     });
 }
+
+function onLoadMore() {
+    cardApiService.getImages().then(imgMarkup);
+}
+
+function clearAImgContainer() {
+    galleryEl.innerHTML = '';
+  }
 
 const imgForm = document.getElementById('search-form');
 imgForm.addEventListener('submit', searchImages);
